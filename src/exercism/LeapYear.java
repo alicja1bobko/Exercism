2a02a5807186e38009abf8694fe67e6640d39aec
AlicjaBobko <alicja_bobko@o2.pl>
2023-06-05 23:35:45 +0200

    isLeapYear

---
+++ src/exercism/LeapYear.java
@@ -0,0 +1,8 @@
+package exercism;
+
+public class LeapYear {
+    public boolean isLeapYear(int year) {
+        return (year % 4 == 0) ? ((year % 100 == 0) ? (year % 400 == 0) : true) : false;
+    }
+}
+
