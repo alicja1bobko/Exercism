1e6ad77d24d36c0ff5062ba555f83c5813b379d7
AlicjaBobko <alicja_bobko@o2.pl>
2023-06-06 23:04:33 +0200

    high scores

---
+++ src/exercism/HighScores.java
@@ -0,0 +1,33 @@
+package exercism;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class HighScores {
+
+    private final List<Integer> highScores;
+
+    public HighScores(List<Integer> highScores) {
+        this.highScores = highScores;
+    }
+
+    List<Integer> scores() {
+        return this.highScores;
+    }
+
+    Integer latest() {
+        return this.highScores.get(highScores.size() - 1);
+    }
+
+    Integer personalBest() {
+        return this.highScores.stream().reduce(Integer::max).orElseThrow();
+    }
+
+    List<Integer> personalTopThree() {
+        List<Integer> sortedList = new ArrayList<>(this.highScores);
+        Collections.sort(sortedList, Collections.reverseOrder());
+        return sortedList.stream().limit(3).toList();
+    }
+
+}
