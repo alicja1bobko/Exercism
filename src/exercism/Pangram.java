5c54cd0b0539d671f47411c680591a164bfcda17
AlicjaBobko <alicja_bobko@o2.pl>
2023-06-06 12:37:11 +0200

    pangram

---
+++ src/exercism/Pangram.java
@@ -0,0 +1,22 @@
+package exercism;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class Pangram {
+    Set<Character> alphabetSet = new HashSet<>(Set.of('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'));
+
+    public boolean isPangram(String input) {
+        Set<Character> lettersSet = new HashSet<>();
+        Pattern pattern = Pattern.compile("[a-zA-Z]+");
+        pattern.matcher(input.toUpperCase())
+                .results()
+                .map(matchResult -> matchResult.group())
+                .collect(Collectors.joining())
+                .chars()
+                .forEach(character -> lettersSet.add((char) character));
+        return lettersSet.equals(alphabetSet);
+    }
+}
